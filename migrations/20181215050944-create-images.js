c765a8c7fb9c1f5898bb0eef91441acfd380565f
Computer Lab Admin <admin@jinnah.edu>
2018-12-15 10:13:32 +0500

    image migration and model

---
+++ migrations/20181215050944-create-images.js
@@ -0,0 +1,39 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('images', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      imagename: {
+        type: Sequelize.STRING
+      },
+      imagepath: {
+        type: Sequelize.STRING
+      },
+      imagesize: {
+        type: Sequelize.DOUBLE
+      },
+      imagetype: {
+        type: Sequelize.STRING
+      },
+      originalpath: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('images');
+  }
+};
