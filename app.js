3b783e3abb404f7ef20a77d9bc76f271b2795260
Shakeeb <khanshakeeb@msn.com>
2018-12-15 09:02:58 +0500

    session store deprecated issue resolved

--- app.js
+++ app.js
@@ -21,7 +21,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({ secret: 'VEvoDOUi21tuAUU' }));
+app.use(session({ 
+    secret: 'VEvoDOUi21tuAUU',
+    name: "classified_site",
+   // store: sessionStore, // connect-mongo session store
+    proxy: true,
+    resave: true,
+    saveUninitialized: true
+  }));
 
 app.use(passport.initialize());
 app.use(passport.session());
